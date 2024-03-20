787a75ae2e664a20e5a5bdb712a251e3d224be49
IsaPesSouza <164249298+IsaPesSouza@users.noreply.github.com>
2024-03-20 17:26:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este código gera um erro quando usado para incorporar a página inicial do Google devido às restrições de segurança.
+//document.body.innerHTML =
+  //'<iframe src="https://pt.pornhub.com/" width="800" height="600"></iframe>'
+ 
